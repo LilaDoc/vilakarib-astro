88ca17b355c877bb935c3503e9722526cfac8711
LYJ <liladocdev@gmail.com>
2025-07-04 09:38:20 +0200

    fix: videoscreen edited video

--- src/components/VideoScreen.jsx
+++ src/components/VideoScreen.jsx
@@ -10,7 +10,7 @@ const VideoScreen = (props) => {
           </button>
         </div>
         <iframe
-          src="https://youtu.be/7pj6ePbFBdU"
+          src="https://www.youtube.com/embed/lK-MzNsLOxU?autoplay=1"
           title="YouTube video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
